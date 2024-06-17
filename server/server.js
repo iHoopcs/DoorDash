70305ddc7bbc23e42d8b7cbfeea74e5435175671
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2024-06-17 17:16:40 -0400

    Migrated data to cloud MongoDB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-//constants
 const express = require('express'); 
-const restaurants = require('./data/restaurants'); 
 const cors = require('cors'); 
+const mongoose = require('mongoose');
+
+const Restaurant = require('./Models/restaurant'); 
+
 
 const app = express(); 
 const port = 3000; 
@@ -9,11 +11,63 @@ const port = 3000;
 //allow http requets from angular frontend
 app.use(cors ({origin: 'http://localhost:4200'})); 
 
-app.listen(port, () => {
-    console.log('Server Started'); 
-})
+
+//MongoDB connection
+mongoose.connect('mongodb+srv://iHoopcs:Bigcts17@cluster1.gtarcla.mongodb.net/DoorDash?retryWrites=true&w=majority&appName=Cluster1')
+    .then(() => [
+        console.log('MongoDB Connected'),
+        app.listen(port, () => {
+            console.log('Server started')
+        })
+ 
+    ])
+    .catch((err) => {
+        console.log(err, 'Connection failed')
+    });
 
 //routes
+app.post('/add-restaurant', (req, res) => {
+    /*
+    const newRestaurant = new Restaurant({
+        name: "",
+        image: "",
+        rating: 0,
+        href: "",
+        hours: "",
+        menu : [
+            {
+                "itemName": "",
+                "itemDescription": "",
+                "price": 0,
+                "itemImg": "",
+                "calories": 0
+            },
+            //add more with template above
+        ],
+        reviews: [
+            {
+                "customer": "",
+                "review": ""
+            },
+            //add more with template above
+        ]
+    })
+    
+    newRestaurant.save()
+        .then((result) => {
+            res.send(result)
+            console.log('*restaurant added')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    */
+
+})
+
+
+
+
 app.get('/', (req, res) => {
     res.redirect('/home'); 
 }); 
